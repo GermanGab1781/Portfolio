f62416545567ecf30fc6d0098967ba315b379319
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-01-19 11:35:17 -0300

    Updated links and images

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -9,23 +9,23 @@ export const projectsData =[
   {
     name:"Budgets App",
     img:require("../media/ProjectBudgets.png"),
-    urlPage:"https://germangab1781.github.io/budgets",
+    urlPage:"https://budgets-khaki.vercel.app/",
     urlCode:"https://github.com/GermanGab1781/budgets",
     languages:["React","UseContext","Local Storage","Bootstrap","CRUD"]
   },
   {
     name:"API Consumption",
     img:require("../media/ProjectMovieDbApi.jpg"),
-    urlPage:"https://germangab1781.github.io/movieAPI",
+    urlPage:"https://movie-api-khaki-one.vercel.app/",
     urlCode:"https://github.com/GermanGab1781/movieAPI",
     languages:["React","Axios","API","Tailwind Css","Framer Motion","SweetAlert","React-router-V6"]
   },
   {
-    name:"Library App",
-    img:require("../media/LibraryProject.jpg"),
-    urlPage:"https://germangab1781.github.io/librarycommerce",
+    name:"Blog App",
+    img:require("../media/ProjectBlogs.jpg"),
+    urlPage:"https://blog-app-gold-two.vercel.app/",
     urlCode:"https://github.com/GermanGab1781/librarycommerce",
-    languages:["React","Node JS","Sequelize","Express","Axios","MySql","E-Commerce","SweetAlert","React-router-V6"]
+    languages:["React","Node JS","Express","Axios","Tailwind Css","Sequelize","MySql","SweetAlert","React-router-V6"]
   },
  
 ]
