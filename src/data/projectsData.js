d032751b2ad5a7b6fe71addff9952b035ee05060
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-10-07 01:00:09 -0300

    Removed uncomplete project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,38 +1,38 @@
-export const projectsData =[
+export const projectsData = [
+  /*  {
+     name:"Ecommerce",
+     img:require("../media/ProjectAlwatoba.png"),
+     urlPage:"https://alwatoba-store.web.app",
+     urlCode:"https://github.com/GermanGab1781/Alwatoba",
+     languages:["React","Firebase","Firebase Auth","Tailwind Css","Framer Motion","React-router-V6","SweetAlert"]
+   }, */
   {
-    name:"Ecommerce",
-    img:require("../media/ProjectAlwatoba.png"),
-    urlPage:"https://alwatoba-store.web.app",
-    urlCode:"https://github.com/GermanGab1781/Alwatoba",
-    languages:["React","Firebase","Firebase Auth","Tailwind Css","Framer Motion","React-router-V6","SweetAlert"]
+    name: "ThreeJs 3D",
+    img: require("../media/BackgroundMain.jpg"),
+    urlPage: "https://three-js-kayn.vercel.app/",
+    urlCode: "https://github.com/GermanGab1781/threeJsKayn",
+    languages: ["React", "ThreeJs", "Tailwind Css", "Framer Motion", "React-router-V6"]
   },
   {
-    name:"ThreeJs 3D",
-    img:require("../media/BackgroundMain.jpg"),
-    urlPage:"https://three-js-kayn.vercel.app/",
-    urlCode:"https://github.com/GermanGab1781/threeJsKayn",
-    languages:["React","ThreeJs","Tailwind Css","Framer Motion","React-router-V6"]
+    name: "Blog App",
+    img: require("../media/ProjectBlogs.jpg"),
+    urlPage: "https://blog-app-gold-two.vercel.app/",
+    urlCode: "https://github.com/GermanGab1781/librarycommerce",
+    languages: ["React", "Node JS", "Express", "Axios", "Tailwind Css", "Sequelize", "MySql", "SweetAlert", "React-router-V6"]
   },
   {
-    name:"Blog App",
-    img:require("../media/ProjectBlogs.jpg"),
-    urlPage:"https://blog-app-gold-two.vercel.app/",
-    urlCode:"https://github.com/GermanGab1781/librarycommerce",
-    languages:["React","Node JS","Express","Axios","Tailwind Css","Sequelize","MySql","SweetAlert","React-router-V6"]
+    name: "API Consumption",
+    img: require("../media/ProjectMovieDbApi.jpg"),
+    urlPage: "https://movie-api-khaki-one.vercel.app/",
+    urlCode: "https://github.com/GermanGab1781/movieAPI",
+    languages: ["React", "Axios", "API", "Tailwind Css", "Framer Motion", "SweetAlert", "React-router-V6"]
   },
   {
-    name:"API Consumption",
-    img:require("../media/ProjectMovieDbApi.jpg"),
-    urlPage:"https://movie-api-khaki-one.vercel.app/",
-    urlCode:"https://github.com/GermanGab1781/movieAPI",
-    languages:["React","Axios","API","Tailwind Css","Framer Motion","SweetAlert","React-router-V6"]
+    name: "Budgets App",
+    img: require("../media/ProjectBudgets.png"),
+    urlPage: "https://budgets-khaki.vercel.app/",
+    urlCode: "https://github.com/GermanGab1781/budgets",
+    languages: ["React", "UseContext", "Local Storage", "Bootstrap", "CRUD"]
   },
-  {
-    name:"Budgets App",
-    img:require("../media/ProjectBudgets.png"),
-    urlPage:"https://budgets-khaki.vercel.app/",
-    urlCode:"https://github.com/GermanGab1781/budgets",
-    languages:["React","UseContext","Local Storage","Bootstrap","CRUD"]
-  },
- 
+
 ]
